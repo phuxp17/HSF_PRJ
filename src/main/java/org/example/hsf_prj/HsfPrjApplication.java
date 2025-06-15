ba388aa9573d08ef28294efbe303733b529c72a0
phuxp17 <phuxp17@gmail.com>
2025-06-15 20:34:52 +0700

    commit

---
+++ src/main/java/org/example/hsf_prj/HsfPrjApplication.java
@@ -0,0 +1,13 @@
+package org.example.hsf_prj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsfPrjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HsfPrjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/hsf_prj/HsfPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hsf_prj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsfPrjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
